49968e1f74d82df1fdcca3049ed5ebaffbe01925
tek <torstenschmits@gmail.com>
2016-08-16 11:01:00 +0200

    refactor `TmuxDispatcher`

--- myo/plugins/tmux/dispatch.py
+++ myo/plugins/tmux/dispatch.py
@@ -1,3 +1,7 @@
+from functools import singledispatch  # type: ignore
+
+from trypnv.machine import Error
+
 from myo.dispatch import Dispatcher
 from myo.command import ShellCommand, Shell
 from myo.plugins.tmux.messages import (TmuxRunCommand, TmuxRunLineInShell,
@@ -5,17 +9,32 @@ from myo.plugins.tmux.messages import (TmuxRunCommand, TmuxRunLineInShell,
 from myo.plugins.command import RunInShell
 
 
+@singledispatch
+def convert_message(msg, options):
+    return Error('invalid message for tmux dispatch: {}'.format(msg))
+
+
+@convert_message.register(RunInShell)
+def _convert_run_in_shell(cmd, options):
+    return TmuxRunLineInShell(cmd.shell, options)
+
+
+@convert_message.register(Shell)  # type: ignore
+def _convert_shell(cmd, options):
+    return TmuxRunShell(cmd, options)
+
+
+@convert_message.register(ShellCommand)  # type: ignore
+def _convert_shell_command(cmd, options):
+    return TmuxRunCommand(cmd, options)
+
+
 class TmuxDispatcher(Dispatcher):
 
     def can_run(self, cmd):
-        return isinstance(cmd, ShellCommand) or isinstance(cmd, RunInShell)
+        return isinstance(cmd, (ShellCommand, RunInShell))
 
     def message(self, cmd, options):
-        if isinstance(cmd, RunInShell):
-            return TmuxRunLineInShell(cmd.shell, options)
-        elif isinstance(cmd, Shell):
-            return TmuxRunShell(cmd, options)
-        else:
-            return TmuxRunCommand(cmd, options)
+        return convert_message(cmd, options)
 
 __all__ = ('TmuxDispatcher',)
