5f30898c885570156f2db5d73f070a959746a0fd
tek <torstenschmits@gmail.com>
2016-08-24 04:43:13 +0200

    refactor python parser

--- myo/output/parser/base.py
+++ myo/output/parser/base.py
@@ -1,11 +1,14 @@
+from collections import namedtuple
 import abc
 
 from myo.logging import Logging
-from myo.output.data import OutputEvent
+from myo.output.data import OutputEvent, OutputEntry
+from myo.record import Record
 
-from amino import List
+from amino import List, Maybe, Try, curried, Just
 
 from ribosome import NvimFacade
+from ribosome.record import re_field, field
 
 
 class ParserBase(Logging):
@@ -16,5 +19,53 @@ class ParserBase(Logging):
     @abc.abstractmethod
     def events(self, output: List[str]) -> List[OutputEvent]:
         ...
+
+
+class EdgeData(Record):
+    r = re_field()
+    entry = field(type)
+
+Step = namedtuple('Step', ['node', 'data'])
+
+
+@curried
+def cons(entry, match):
+    return Try(entry, text=match.string, **match.groupdict()).to_maybe
+
+
+class SimpleParser(ParserBase):
+
+    @property
+    def graph(self):
+        ...
+
+    def edges(self, node):
+        e = (Step(t, d['data']) for f, t, d in self.graph.edges(node,
+                                                                data=True))
+        return List.wrap(e)
+
+    def _process(self, node: str, output: List[str], result: List[OutputEvent],
+                 current: List[OutputEntry]) -> List[OutputEvent]:
+        def add_event():
+            ev = OutputEvent(head='traceback', entries=current)
+            new = current.empty.no.maybe(ev).to_list
+            return result + new
+        def parse_line(line: str, rest: List[str]):
+            def match(step: Step):
+                return (
+                    Maybe(step.data.r.match(line)) //
+                    cons(step.data.entry)
+                ) & Just(step.node)
+            def cont(entry: OutputEntry, next_node: str):
+                return self._process(next_node, rest, result,
+                                     current.cat(entry))
+            def next_event():
+                new_output = rest if node == 'start' else output
+                return self._process('start', new_output, add_event(), List())
+            return self.edges(node).find_map(match).map2(cont) | next_event
+        return output.detach_head.map2(parse_line) | add_event()
+
+    def events(self, output):
+        return self._process('start', output, List(), List())
 
 __all__ = ('ParserBase',)
--- myo/output/parser/python.py
+++ myo/output/parser/python.py
@@ -1,40 +1,25 @@
-from collections import namedtuple
-
 from networkx import DiGraph
 
-from amino import List, Maybe, Just, curried, Try
 from amino.lazy import lazy
 
-from ribosome.record import field, re_field
+from ribosome.record import field
 
-from myo.output.parser.base import ParserBase
-from myo.record import Record
-from myo.output.data import PositionEntry, OutputEntry, ErrorEntry, OutputEvent
+from myo.output.data import PositionEntry, ErrorEntry, OutputEntry
+from myo.output.parser.base import EdgeData, SimpleParser
 
 
 class FileEntry(PositionEntry):
     fun = field(str)
 
-
-class EdgeData(Record):
-    r = re_field()
-    entry = field(type)
-
 _file = EdgeData(
     r='  File "(?P<path>.+)", line (?P<line>\d+), in (?P<fun>\S+)',
-    entry=FileEntry)
+    entry=FileEntry
+)
 _expr = EdgeData(r='    (.+)', entry=OutputEntry)
 _error = EdgeData(r='(?P<error>\S+): (?P<msg>\S+)', entry=ErrorEntry)
 
-Step = namedtuple('Step', ['node', 'data'])
 
-
-@curried
-def cons(entry, match):
-    return Try(entry, text=match.string, **match.groupdict()).to_maybe
-
-
-class Parser(ParserBase):
+class Parser(SimpleParser):
 
     @lazy
     def graph(self):
@@ -44,34 +29,5 @@ class Parser(ParserBase):
         g.add_edge('expr', 'file', data=_file)
         g.add_edge('expr', 'error', data=_error)
         return g
-
-    def edges(self, node):
-        e = (Step(t, d['data']) for f, t, d in self.graph.edges(node,
-                                                                data=True))
-        return List.wrap(e)
-
-    def _process(self, node: str, output: List[str], result: List[OutputEvent],
-                 current: List[OutputEntry]) -> List[OutputEvent]:
-        def add_event():
-            ev = OutputEvent(head='traceback', entries=current)
-            new = current.empty.no.maybe(ev).to_list
-            return result + new
-        def parse_line(line: str, rest: List[str]):
-            def match(step: Step):
-                return (
-                    Maybe(step.data.r.match(line)) //
-                    cons(step.data.entry)
-                ) & Just(step.node)
-            def cont(entry: OutputEntry, next_node: str):
-                return self._process(next_node, rest, result,
-                                     current.cat(entry))
-            def next_event():
-                new_output = rest if node == 'start' else output
-                return self._process('start', new_output, add_event(), List())
-            return self.edges(node).find_map(match).map2(cont) | next_event
-        return output.detach_head.map2(parse_line) | add_event()
-
-    def events(self, output):
-        return self._process('start', output, List(), List())
 
 __all__ = ('Parser',)
