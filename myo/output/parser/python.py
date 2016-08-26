d324b29792ea8b2129d486e5bf9f52182baf871c
tek <torstenschmits@gmail.com>
2016-08-26 14:34:30 +0200

    fold exprs into `FileEntry` in python parser

--- myo/output/parser/python.py
+++ myo/output/parser/python.py
@@ -1,21 +1,35 @@
+from typing import Tuple
+
 from networkx import DiGraph
 
 from amino.lazy import lazy
+from amino import List, Empty, Just, Maybe, __, L, _
 
-from ribosome.record import field
+from ribosome.record import field, maybe_field
 
-from myo.output.data import PositionEntry, ErrorEntry, OutputEntry
+from myo.output.data import (PositionEntry, ErrorEntry, OutputEntry,
+                             OutputEvent, OutputLine)
 from myo.output.parser.base import EdgeData, SimpleParser
 
 
 class FileEntry(PositionEntry):
     fun = field(str)
+    expr = maybe_field(str)
+
+    @property
+    def output_lines(self):
+        x = self.expr / L(OutputLine.create)(_, self)
+        return super().output_lines + x.to_list
+
+
+class ExprEntry(OutputEntry):
+    pass
 
 _file = EdgeData(
     r='  File "(?P<path>.+)", line (?P<line>\d+), in (?P<fun>\S+)',
     entry=FileEntry
 )
-_expr = EdgeData(r='    (.+)', entry=OutputEntry)
+_expr = EdgeData(r='    (.+)', entry=ExprEntry)
 _error = EdgeData(r='(?P<error>\S+): (?P<msg>\S+)', entry=ErrorEntry)
 
 
@@ -29,5 +43,20 @@ class Parser(SimpleParser):
         g.add_edge('expr', 'file', data=_file)
         g.add_edge('expr', 'error', data=_error)
         return g
+
+    def event(self, entries: List[OutputEntry]):
+        def folder(z: Tuple[List[OutputEntry], Maybe[FileEntry]], a):
+            res, cur = z
+            add, new = (
+                (cur, Just(a))
+                if isinstance(a, FileEntry) else
+                ((cur / __.set(expr=Just(a.text))).or_else(Just(a)), Empty())
+                if isinstance(a, ExprEntry) else
+                (Just(a), Empty())
+            )
+            return res + add.to_list, new
+        grouped, rest = entries.fold_left((List(), Empty()))(folder)
+        complete = grouped + rest.to_list
+        return Just(OutputEvent(head='traceback', entries=complete))
 
 __all__ = ('Parser',)
