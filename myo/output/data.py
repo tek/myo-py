e7e1ce5bb78c7ec751c3957f43e043e40e927da3
tek <torstenschmits@gmail.com>
2016-08-26 14:33:49 +0200

    `OutputLine` data for parsing that maps displayed lines to entries

--- myo/output/data.py
+++ myo/output/data.py
@@ -1,6 +1,7 @@
-from amino import _, Path
+from amino import _, Path, List
+from amino.lazy import lazy
 
-from ribosome.record import field, list_field, dfield
+from ribosome.record import field, list_field, dfield, any_field
 
 from myo.record import Record
 from myo.util import parse_int
@@ -10,8 +11,17 @@ class OutputEntry(Record):
     text = field(str)
 
     @property
-    def formatted(self):
-        return self.text
+    def output_lines(self):
+        return List(OutputLine.create(self.text, self))
+
+
+class OutputLine(Record):
+    text = field(str)
+    target = any_field()
+
+    @staticmethod
+    def create(text, target):
+        return OutputLine(text=text, target=target)
 
 
 class ErrorEntry(OutputEntry):
@@ -20,10 +30,7 @@ class ErrorEntry(OutputEntry):
 
 
 def _parse_line(data):
-    if isinstance(data, int):
-        return data
-    else:
-        return parse_int(data) | 0
+    return data if isinstance(data, int) else parse_int(data) | 0
 
 
 class PositionEntry(OutputEntry):
@@ -37,16 +44,29 @@ class OutputEvent(Record):
     entries = list_field(OutputEntry)
 
     @property
-    def display_lines(self):
-        return (self.entries / _.formatted).cons(self.head)
+    def lines(self):
+        return (
+            (self.entries // _.output_lines)
+            .cons(OutputLine.create(self.head, self))
+        )
 
 
 class ParseResult(Record):
     head = field(str)
     events = list_field(OutputEvent)
 
+    @lazy
+    def lines(self):
+        return (
+            (self.events // _.lines)
+            .cons(OutputLine.create(self.head, self))
+        )
+
     @property
     def display_lines(self):
-        return (self.events // _.display_lines).cons(self.head)
+        return self.lines / _.text
+
+    def target_for_line(self, line):
+        return self.lines.lift(line) / _.target
 
 __all__ = ('OutputEntry', 'OutputEvent', 'ParseResult')
