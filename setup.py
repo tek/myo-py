7423ee07bd0fd2f3555c84a15fb3b7267582b8ed
tek <torstenschmits@gmail.com>
2016-08-29 04:48:52 +0200

    0.1.0

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup, find_packages
 
+version_parts = (0, 1, 0)
+version = '.'.join(map(str, version_parts))
+
 setup(
     name='myo',
     description='command and layout management for neovim',
-    version='0.0.2',
+    version=version,
     author='Torsten Schmits',
     author_email='torstenschmits@gmail.com',
     license='MIT',
