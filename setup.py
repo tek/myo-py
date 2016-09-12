5f2f6d5f46ecbdd398c45838ee6f8f082ccb222f
tek <torstenschmits@gmail.com>
2016-09-12 04:00:54 +0200

    bump ribosome

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     url='https://github.com/tek/myo',
     packages=find_packages(exclude=['unit', 'unit.*']),
     install_requires=[
-        'ribosome>=8.5.0',
+        'ribosome>=9.0.0',
         'libtmux',
         'psutil',
         'networkx',
