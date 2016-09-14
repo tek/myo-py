c719b6dc3d652447795ff6c966a6a707488447aa
tek <torstenschmits@gmail.com>
2016-09-14 19:21:19 +0200

    type check native tmux object

--- myo/ui/tmux/adapter.py
+++ myo/ui/tmux/adapter.py
@@ -1,9 +1,14 @@
+from libtmux.common import TmuxRelationalObject
+
 from myo.logging import Logging
 
 
 class Adapter(Logging):
 
     def __init__(self, native) -> None:
+        if not isinstance(native, TmuxRelationalObject):
+            raise Exception(
+                'passed {!r} to {}'.format(native, self.__class__.__name__))
         self.native = native
 
     def __repr__(self):
