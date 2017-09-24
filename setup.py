f165c10dc246142259a638f508ec6a5969086219
Torsten Schmits <torstenschmits@gmail.com>
2017-09-24 18:51:17 +0200

    0.34.8

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version_parts = (0, 34, 7)
+version_parts = (0, 34, 8)
 version = '.'.join(map(str, version_parts))
 
 setup(
@@ -13,7 +13,7 @@ setup(
     url='https://github.com/tek/myo',
     packages=find_packages(exclude=['unit', 'unit.*', 'integration', 'integration.*']),
     install_requires=[
-        'ribosome==11.2.8',
+        'ribosome==11.2.9',
         'libtmux',
         'psutil',
         'networkx',
