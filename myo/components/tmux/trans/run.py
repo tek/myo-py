249e800ae51826955ec634a7767c749fac91df85
Torsten Schmits <torstenschmits@gmail.com>
2018-02-27 21:48:47 +0100

    refactor `run_task_in_pane`

--- myo/components/tmux/trans/run.py
+++ myo/components/tmux/trans/run.py
@@ -1,11 +1,11 @@
-from amino import do, Do, Boolean
+from amino import do, Do, Boolean, Path, Nothing, Just, L, _, Maybe
 from amino.lenses.lens import lens
 from amino.dispatch import PatMat
 
 from chiasma.io.state import TS
 from chiasma.data.tmux import TmuxData
 from chiasma.pane import pane_by_ident
-from chiasma.commands.pane import send_keys
+from chiasma.commands.pane import send_keys, pipe_pane
 
 from ribosome.trans.api import trans
 from ribosome.dispatch.component import ComponentData
@@ -13,6 +13,16 @@ from ribosome.trans.action import TransM
 
 from myo.env import Env
 from myo.command.run_task import RunTaskDetails, UiSystemTaskDetails, UiShellTaskDetails, RunTask
+from myo.data.command import Command
+from myo.ui.data.view import Pane
+
+
+@do(TS[ComponentData[Env, TmuxData], None])
+def run_system_task(cmd: Command, pane: Pane, log_path: Maybe[Path]) -> Do:
+    tpane = yield pane_by_ident(pane.ident).zoom(lens.comp)
+    id = yield TS.from_maybe(tpane.id, f'no tmux id for pane {pane.ident}')
+    yield log_path / L(pipe_pane)(id, _) / TS.lift | TS.unit
+    yield TS.lift(send_keys(id, cmd.lines))
 
 
 class run_task_in_pane(PatMat, alg=RunTaskDetails):
@@ -22,22 +32,14 @@ class run_task_in_pane(PatMat, alg=RunTaskDetails):
 
     @do(TS[ComponentData[Env, TmuxData], None])
     def ui_system_task_details(self, details: UiSystemTaskDetails) -> Do:
-        cmd = self.task.command
-        pane = details.pane
-        tpane = yield pane_by_ident(pane.ident).zoom(lens.comp)
-        id = yield TS.from_maybe(tpane.id, f'no tmux id for pane {pane.ident}')
-        yield TS.lift(send_keys(id, cmd.lines))
+        yield run_system_task(self.task.command, details.pane, Just(self.task.log))
 
     @do(TS[ComponentData[Env, TmuxData], None])
     def ui_shell_task_details(self, details: UiShellTaskDetails) -> Do:
-        cmd = self.task.command
-        pane = details.pane
-        tpane = yield pane_by_ident(pane.ident).zoom(lens.comp)
-        id = yield TS.from_maybe(tpane.id, f'no tmux id for pane {pane.ident}')
-        yield TS.lift(send_keys(id, cmd.lines))
+        yield run_system_task(self.task.command, details.pane, Just(self.task.log))
 
     def patmat_default(self, task: RunTaskDetails) -> TS:
-        yield TS.error('aaahhhh')
+        yield TS.error(f'cannot run {task} in tmux')
 
 
 @trans.free.unit(trans.st)
