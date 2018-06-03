477672973ed661e110bc3c99d9a81365c8b41e55
Torsten Schmits <torstenschmits@gmail.com>
2018-06-03 17:42:56 +0200

    wait for pid when running command in tmux

--- myo/components/tmux/compute/run.py
+++ myo/components/tmux/compute/run.py
@@ -1,4 +1,4 @@
-from amino import do, Do, Boolean, Path, Just, L, _, Maybe
+from amino import do, Do, Boolean, Path, Just, L, _, Maybe, Nothing
 from amino.lenses.lens import lens
 from amino.case import Case
 from amino.logging import module_log
@@ -8,6 +8,8 @@ from myo.components.tmux.data import TmuxData
 
 from chiasma.pane import pane_by_ident
 from chiasma.commands.pane import send_keys, pipe_pane
+from chiasma.io.util import tmuxio_repeat_timeout
+from chiasma.io.compute import TmuxIO
 
 from ribosome.compute.api import prog
 from ribosome.config.component import ComponentData
@@ -22,13 +24,18 @@ from myo.tmux.pid import process_pid
 log = module_log()
 
 
+# FIXME wait for pid?
 @do(TS[ComponentData[Env, TmuxData], Maybe[Pid]])
 def run_system_task(cmd: Command, pane: Pane, log_path: Maybe[Path]) -> Do:
     tpane = yield pane_by_ident(pane.ident).zoom(lens.comp.views)
     id = yield TS.from_maybe(tpane.id, f'no tmux id for pane {pane.ident}')
-    yield log_path / L(pipe_pane)(id, _) / TS.lift | TS.unit
+    yield log_path.map(lambda a: TS.lift(pipe_pane(id, a))).get_or_strict(TS.unit)
     yield TS.lift(send_keys(id, cmd.lines))
-    yield TS.lift(process_pid(id))
+    yield TS.lift(TmuxIO.flush())
+    yield TS.lift(
+        tmuxio_repeat_timeout(lambda: process_pid(id), lambda a: a.is_just, '', timeout=3.)
+        .recover_error(lambda a: Nothing)
+    )
 
 
 class run_task_in_pane(Case[RunTaskDetails, Maybe[Pid]], alg=RunTaskDetails):
@@ -51,16 +58,11 @@ class run_task_in_pane(Case[RunTaskDetails, Maybe[Pid]], alg=RunTaskDetails):
 
 @prog
 @do(NS[ComponentData[Env, TmuxData], Maybe[Pid]])
-def run_in_pane(task: RunTask) -> Do:
+def tmux_run_command(task: RunTask) -> Do:
     yield run_task_in_pane(task)(task.details).nvim
 
 
-@prog.do(Maybe[Pid])
-def tmux_run_command(task: RunTask) -> Do:
-    yield run_in_pane(task)
-
-
-def tmux_can_run(task: RunTaskDetails) -> Boolean:
+def tmux_can_run(task: RunTask) -> bool:
     return isinstance(task.details, (UiSystemTaskDetails, UiShellTaskDetails))
 
 
