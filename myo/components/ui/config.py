33be420d785e597460cb22768d95c048ab78b8ca
Torsten Schmits <torstenschmits@gmail.com>
2018-10-12 17:06:17 +0200

    doc for `create_pane`

--- myo/components/ui/config.py
+++ myo/components/ui/config.py
@@ -1,4 +1,4 @@
-from amino import List
+from amino import List, Map, Just
 from amino.boolean import true
 
 from ribosome.config.component import Component
@@ -18,16 +18,25 @@ from myo.components.ui.compute.toggle_layout import toggle_layout
 from myo.components.ui.compute.kill_pane import kill_pane
 from myo.components.ui.compute.focus import focus
 
-create_pane_help = '''Configure a new pane that can be spawned with `MyoOpenPane`.
-'''
+create_pane_help = DocBlock.string('''Configure a new pane that can be spawned with `MyoOpenPane`.''')
+create_pane_json_help = Map(
+    layout='containing layout name',
+    ident='name to be used for commands like MyoOpenPane',
+    min_size='minimum size integer',
+    max_size='maximum size integer',
+    fixed_size='fixed size, combination of min and max',
+    minimized_size='size after calling MyoMinimizePane',
+    weight='amount of growth this pane gets when there is space left above the minimums',
+    position='absolute position in the layout',
+    minimized='whether to create the pane in minimized state',
+)
 
 
 ui = Component.cons(
     'ui',
     state_type=UiData,
     rpc=List(
-        # rpc.write(create_pane).conf(json=true, help=DocBlock.string(create_pane_help)),
-        rpc.write(create_pane).conf(json=true),
+        rpc.write(create_pane).conf(json=true, help=create_pane_help, json_help=Just(create_pane_json_help)),
         rpc.write(open_pane).conf(json=true),
         rpc.write(close_pane),
         rpc.write(toggle_pane),
