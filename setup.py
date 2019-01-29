861e80475ccbf6328b5ea7197dda7a9e808ec7a9
Torsten Schmits <torstenschmits@gmail.com>
2019-01-29 16:01:44 +0100

    1.0.2.a.7

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version_parts = (1, 0, 2, 'a', 6)
+version_parts = (1, 0, 2, 'a', 7)
 version = '.'.join(map(str, version_parts))
 
 setup(  # type: ignore
